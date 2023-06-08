9a57ee611280ac64f4225e944a9135f14ae6c32c
Giglio59 <antoinegiglioo@gmail.com>
2023-06-08 14:40:35 +0200

    modification du logo menu sur toute les page

--- link/main.js
+++ link/main.js
@@ -27,3 +27,34 @@ menuItems.forEach(function(menuItem) {
   });
 });
 
+//fetch("../link/kombucha.json")//
+//.then(data => data.json())//
+//.then(data => {//
+ // console.log(data);//
+//})//
+
+ // Import the functions you need from the SDKs you need
+ //import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+ 
+ //import { getFirestore , doc, getDoc, where } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ //const firebaseConfig = {
+//  // apiKey: "AIzaSyAm1sokCESHXHVkBUvSDnBQ2O0m1N3qRzo",
+ //  authDomain: "kombucha-4df74.firebaseapp.com",
+ //  projectId: "kombucha-4df74",
+ //  storageBucket: "kombucha-4df74.appspot.com",
+  // messagingSenderId: "631140585454",
+  // appId: "1:631140585454:web:51edaeed097896faa448ff",
+  // measurementId: "G-1JKPLLYT1C"
+ //};
+
+ // Initialize Firebase
+ //const app = initializeApp(firebaseConfig);
+ //const fs = getFirestore(app);
+ //const db = doc(fs, "kamboucha", "PTmQpqxfnOGtlOxfCCuo");
+ //const docSnap = await getDoc(db)
+ //console.log(docSnap.data());
